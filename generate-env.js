e97b41cd874a01b4890d039c6a00dcc2f47a66ac
Vineett09 <152068376+Vineett09@users.noreply.github.com>
2025-06-09 16:05:48 +0530

    fix

--- generate-env.js
+++ generate-env.js
@@ -4,6 +4,7 @@ const content = `
 window.env = {
   EMAILJS_SERVICE_ID: "${process.env.EMAILJS_SERVICE_ID}",
   EMAILJS_TEMPLATE_ID: "${process.env.EMAILJS_TEMPLATE_ID}",
+  EMAILJS_PUBLIC_KEY: "${process.env.EMAILJS_PUBLIC_KEY}",
 };
 `;
 
